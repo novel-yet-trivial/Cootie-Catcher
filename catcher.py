6cc2d172fa3752236dcde8df9b220215a0105ee7
novel-yet-trivial <jonsspamaccout@gmail.com>
2016-10-28 11:08:20 -0700

    A lot of cleanup

--- catcher.py
+++ catcher.py
@@ -1,9 +1,32 @@
 '''Cootie Catcher Program that take user input to give them a function.
-Built using Python 3.5 on Windows 10'''
+Built using Python 3.5 on Windows 10
+Runs on any system with python 2 or 3'''
+
+#add python2 support
+from __future__ import print_function
+try:
+    input = raw_input
+except NameError:
+    pass
 
 import os
 import random
 from colorama import Fore, Style
+
+#add linux/mac support
+import sys
+if sys.platform.startswith('win'):
+    CLEAR_COMMAND = 'cls'
+else:
+    CLEAR_COMMAND = 'clear'
+
+# read fortune constants.
+with open('Fortunes.txt') as reader:
+    F_FORTUNES = reader.readlines()    #Taking the fortunes out of the random document
+with open('Yes_No.txt') as reader:
+    YN_FORTUNES = reader.readlines()   #Taking the foutunes out of the yes_no document
+with open('Trivia.txt') as reader:
+    T_FORTUNES = reader.readlines()    #Taking the fortunes out of the trivia document
 
 def intro():
     '''Header for the program, that will show up for the whole game'''
@@ -14,73 +37,75 @@ def intro():
     print("--------------------------------")
     print("\n")
 
-def open_docs():
-    '''
-       Opening the fortune documents and putting them into lists
-    '''
-    with open('Fortunes.txt') as reader:
-        f_fortunes = reader.readlines()    #Taking the fortunes out of the random document
-    with open('Yes_No.txt') as reader:
-        yn_fortunes = reader.readlines()   #Taking the foutunes out of the yes_no document
-    with open('Trivia.txt') as reader:
-        t_fortunes = reader.readlines()    #Taking the fortunes out of the trivia document
-    return(f_fortunes, yn_fortunes, t_fortunes)
+#yes this tiny function seems trivial but it is normal and pythonic
+def choose_from_list(choice_list):
+    """
+    :param choice_list:
+      a list of strings to choose from
+    :return:
+      a list of 8 random values from the choice_list, plus an empty string
+    """
+    return [random.choice(choice_list) for _ in range(8)] + [""]
 
-def choose_fortune(choose, f_fortunes, yn_fortunes, t_fortunes):
+def choose_from_user_input():
+    """
+    :return:
+      a list of 8 user inputted strings, shuffled, plus an empty string
+    """
+    os.system(CLEAR_COMMAND)
+    fortune = list()
+    words = ['first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh', 'last']
+    for word in words:
+        #Word list to make sure the correct position is being referenced to the player
+        intro()
+        fortune.append(input("Enter your %s fortune: " % word))
+        #Adding the individual fortunes
+        os.system(CLEAR_COMMAND)
+    random.shuffle(fortune)  #Finalizing the fortune list for the custom fortunes
+    fortune.append("")
+
+def choose_fortune(choose):
     '''
       There are four different fortune types that can be accessed
       This allows the choice between Random, Trivia, Yes_No Question or Custom)
+      :param choose:
+        string from the user indicating the catagory
+      :return:
+        a list of 8 strings plus a optional question.
     '''
-    fortune = list()
     if choose.lower() == "choose":
-        os.system('cls')
-        for counter in range(0, 8):
-            word = ['first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh', 'last']
-            #Word list to make sure the correct position is being referenced to the player
-            intro()
-            fortune.insert(counter, '"' + input("Enter your %s fortune: " % word[counter]).strip() + '"')
-            #Adding the individual fortunes
-            os.system('cls')
-        random.shuffle(fortune)  #Finalizing the fortune list for the custom fortunes
-        fortune.append("")
+        fortune = choose_from_user_input()
     if choose.lower() == "random":
-        for counter in range(0, 8):
-            #Creating the fortune list for the random fortunes
-            fortune.insert(counter, random.choice(f_fortunes))
-        fortune.append("")
+        fortune = choose_from_list(F_FORTUNES)
     if choose.lower() == "trivia":
-        for counter in range(0, 8):
-            #Creating the fortune list for the trivia fortunes
-            fortune.insert(counter, random.choice(t_fortunes))
-        fortune.append("")
+        fortune = choose_from_list(T_FORTUNES)
     if choose.lower() == "question":
-        for counter in range(0, 8):
-            #Creating the fortune list for the yes_no fortunes
-            fortune.insert(counter, random.choice(yn_fortunes))
+        fortune = choose_from_list(YN_FORTUNES)
         question = input("\nWhat is your question? ").strip()
         #Adding the asked question to the fortunes list, so it can be called back later
-        fortune.append(question)
+        fortune[-1] = question #replace the last value
     return fortune
 
-def red(arg):
-    ''' Function used for turning specified strings BRIGHT RED '''
-    response = (Fore.RED + Style.BRIGHT + arg + Style.RESET_ALL)
-    return response
+#this may be a little confusing to you. We make a function that makes a function.
+#(This is called a lambda or closure in CS)
+#See how this means if we want to remove Style.BRIGHT or something, there is
+#a single place in the code that needs to be modified
+def make_color_func(color):
+    """makes a closure to return a colored string
+    :param color:
+      ANSI color string
+    :return:
+      function that sets an argument to that color
+    """
+    def color_func(arg):
+        return color + Style.BRIGHT + arg + Style.RESET_ALL
+    return color_func
 
-def blue(arg):
-    ''' Function used for turning specified strings BRIGHT BLUE '''
-    response = (Fore.BLUE + Style.BRIGHT + arg  + Style.RESET_ALL)
-    return response
-
-def green(arg):
-    ''' Function used for turning specified strings BRIGHT GREEN '''
-    response = (Fore.GREEN + Style.BRIGHT + arg  + Style.RESET_ALL)
-    return response
-
-def yellow(arg):
-    ''' Function used for turning specified strings BRIGHT YELLOW '''
-    response = (Fore.YELLOW + Style.BRIGHT + arg  + Style.RESET_ALL)
-    return response
+#these are equivalent to def statements
+red = make_color_func(Fore.RED)
+blue = make_color_func(Fore.BLUE)
+green = make_color_func(Fore.GREEN)
+yellow = make_color_func(Fore.YELLOW)
 
 def string_moves(color):
     '''
@@ -97,6 +122,35 @@ def string_moves(color):
         response += yellow('Y...E...L...L...O...W...') +'\n' #Turns response yellow
     return response
 
+def user_choice(choice_list, question=">", prompt=None):
+    """
+    presents the user with a list of choices and asks them to pick one
+    :param choice_list:
+      list of strings to choose from; must all be lowercase
+    :param question:
+      string to be asked in the input call
+    :param prompt:
+      optional additional string to be printed before the input call
+    :return:
+      the users choice, lowercase, stripped of whitespace
+    """
+    if prompt:
+        print(prompt)
+    choice = input(question).strip()
+    while choice not in choice_list:
+        print("\nInvalid Choice\n")
+        if prompt:
+            print(prompt)
+        choice = input(question).strip()
+    return choice
+
+def user_choice_num(choice_list):
+    """
+    presents the user with a list of numbers and asks them to pick one
+    """
+    question = "Choose {}: ".format(", ".join(choice_list))
+    return user_choice(choice_list, question)
+
 def num(color, number):
     '''
         Function to determine what numbers will be displayed next
@@ -105,25 +159,14 @@ def num(color, number):
         Used color length because it is easier to just deal with even and odd
     '''
     if len(color) % 2 == 0 and number == 0:
-        choose_num = str(input("Choose 1, 4, 5, 8: ")).strip()
-        while choose_num not in ['1', '4', '5', '8']:
-            print("\nInvalid Choice\n")
-            choose_num = str(input("Choose 1, 4, 5, 8: ")).strip()
+        choose_num = user_choice_num(['1', '4', '5', '8'])
     elif number % 2 == 0 and len(color) % 2 == 0 or number % 2 == 1 and len(color) % 2 == 1:
-        choose_num = str(input("Choose 1, 4, 5, 8: ")).strip()
-        while choose_num not in ['1', '4', '5', '8']:
-            print("\nInvalid Choice\n")
-            choose_num = str(input("Choose 1, 4, 5, 8: ")).strip()
+        choose_num = user_choice_num(['1', '4', '5', '8'])
     elif len(color) % 2 == 1 and number == 0:
-        choose_num = str(input("Choose 2, 3, 6, 7: ")).strip()
-        while choose_num not in ['2', '3', '6', '7']:
-            print("\nInvalid Choice\n")
-            choose_num = str(input("Choose 2, 3, 6, 7: ")).strip()
+        choose_num = user_choice_num(['2', '3', '6', '7'])
     elif number % 2 == 1 and len(color) % 2 == 0 or number % 2 == 0 and len(color) % 2 == 1:
-        choose_num = str(input("Choose 2, 3, 6, 7: ")).strip()
-        while choose_num not in ['2', '3', '6', '7']:
-            print("\nInvalid Choice\n")
-            choose_num = str(input("Choose 2, 3, 6, 7: ")).strip()
+        choose_num = user_choice_num(['2', '3', '6', '7'])
+    #WARNING: you have no else case! are you sure all bases are covered?
     numint = int(choose_num)
     return numint
 
@@ -161,56 +204,54 @@ def fortune_print(arg, fortune):
     print(fortune[arg-1])
     print('\n')
 
+def change_fortune():
+    os.system(CLEAR_COMMAND)
+    intro()
+    prompt = "Would you like to Choose your own fortunes / Get Random ones / Get Trivia facts / Ask specific Question? \n"
+    question = "Enter Choose, Random, Trivia or Question: "
+    choice_list = ['choose', 'random', 'trivia', 'question']
+    fortune_choice = user_choice(choice_list, question, prompt)
+
+    # Creates the fortunes based on fortune type
+    fortune = choose_fortune(fortune_choice)
+    return fortune
+
+def play(fortune):
+    """play a single round"""
+    os.system(CLEAR_COMMAND)
+    intro()
+    print_question(fortune)  #If a question was asked, it will be printed here
+
+    #Asking the player to pick a color
+    question = "Choose %s, %s, %s or %s: " %(red('Red'), blue('Blue'), green('Green'), yellow('Yellow'))
+    color = user_choice(['yellow', 'blue', 'green', 'red'], question)
+
+    string_move = string_moves(color.lower()) #Showing how the catcher 'moves'
+    print(string_move)
+    numint = num(color, 0) #Asking the player to pick a number
+    int_move = int_moves(numint, color.lower()) #Showing how the catcher 'moves'
+    print(int_move)
+    num2int = num(color, numint) #Asking the player to pick another number
+    int2_move = int_moves(num2int, color.lower()) #Showing how the catcher 'moves'
+    print(int2_move)
+    print_question(fortune) #If a question was asked, it will be printed again
+    fortune_print(num2int, fortune)
+
 def cootie():
     '''Main function that puts everything together'''
-    intro()
-    again = 'y' #Initializing that the player wants to play the first time
-    change = 'y' #Initializing the the player wants to choose their first fortune type
-    while again == 'y':  #Loop makes sure the game keeps going until the player tells it to stop
-        if change == 'y':  #Gives the player the option to pick a different fortune type
-            #Opens the fortune documents and populate fortune lists
-            f_fortunes, yn_fortunes, t_fortunes = open_docs()
-            print("Would you like to Choose your own fortunes / Get Random ones / Get Trivia facts / Ask specific Question? \n")
-            fortune_choice = input("Enter Choose, Random, Trivia or Question: ")
-            # Creates the fortunes based on fortune type
-            fortune = choose_fortune(fortune_choice, f_fortunes, yn_fortunes, t_fortunes)
-            while fortune_choice.lower() not in ['choose', 'random', 'trivia', 'question']:
-                print("\nInvalid Choice\n")
-                print("Would you like to Choose your own fortunes / Get Random ones / Get Trivia facts / Ask specific Question? \n")
-                fortune_choice = input("Enter Choose, Random, Trivia or Question: ").strip()
+    fortune = change_fortune()
+    while True:  #Loop makes sure the game keeps going until the player tells it to stop
+        play(fortune)
 
-        os.system('cls')
-        intro()
-        print_question(fortune)  #If a question was asked, it will be printed here
-    #Asking the player to pick a color
-        color = input("Choose %s, %s, %s or %s: " %(red('Red'), blue('Blue'), green('Green'), yellow('Yellow'))).strip()
-        while color.lower() not in ['yellow', 'blue', 'green', 'red']:
-            print("\nInvalid Choice\n")
-            color = input("Choose %s, %s, %s or %s: " %(red('Red'), blue('Blue'), green('Green'), yellow('Yellow'))).strip()
-        string_move = string_moves(color.lower()) #Showing how the catcher 'moves'
-        print(string_move)
-        numint = num(color, 0) #Asking the player to pick a number
-        int_move = int_moves(numint, color.lower()) #Showing how the catcher 'moves'
-        print(int_move)
-        num2int = num(color, numint) #Asking the player to pick another number
-        int2_move = int_moves(num2int, color.lower()) #Showing how the catcher 'moves'
-        print(int2_move)
-        print_question(fortune) #If a question was asked, it will be printed again
-        fortune_print(num2int, fortune)
-    #Choice of if player wants to keep playing
-        again = input("Would you like to play again? Y or N: ").lower().strip()
-        while again not in ['y', 'n']:
-            print("\nInvalid Choice\n")
-            again = input("Would you like to play again? Y or N: ").lower().strip()
+        #Choice of if player wants to keep playing
+        again = user_choice(['y', 'n'], "Would you like to play again? Y or N:")
         if again == 'n': #If the player chooses no the loop breaks
             break
-    #Choice of if the player wants new fortunes
-        change = input("Would you like to change your fortunes or question? Y or N? ").lower().strip()
-        while change not in ['y', 'n']:
-            print("\nInvalid Choice\n")
-            change = input("Would you like to change your fortunes or question? Y or N? ").lower().strip()
-        os.system('cls')
-        intro()
 
-os.system('cls')
-cootie()  #Running the code
+        #Choice of if the player wants new fortunes
+        change = user_choice(['y', 'n'], "Would you like to change your fortunes or question? Y or N? ")
+        if change == 'y':  #Gives the player the option to pick a different fortune type
+            fortune = change_fortune()
+
+if __name__ == "__main__":
+    cootie()  #Running the code
